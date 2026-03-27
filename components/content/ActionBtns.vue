d7b01286283a89d0b7ead09a1cf177359d3bf355
Maxim <95909662+mtimshin@users.noreply.github.com>
2026-03-27 11:20:21 +0300

    fix: restore btns block via dedicated MDC component

---
+++ components/content/ActionBtns.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="cont">
+    <div class="btns">
+      <a href="#stages" class="btn">Этапы работ</a>
+      <a href="#contacts" class="btn2">Связаться со мной</a>
+    </div>
+  </div>
+</template>
